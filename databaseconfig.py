5fc88030e5ede01b1f316ca094c462ce1ce3d233
Ben Paysen <bpaysen@pws.bz>
2018-06-03 19:52:26 -0700

    clean remaining cached files .gitignore

--- databaseconfig.py
+++
@@ -1,8 +0,0 @@
-import pymysql.cursors
-
-connection = pymysql.connect(host='127.0.0.1',
-							user='root',
-							password='Julian',
-							db='UserTest_db',
-							charset='utf8mb4',
-							cursorclass=pymysql.cursors.DictCursor)
